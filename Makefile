fb7831fb2cfbe6113cc30612a24829bcc4de6012
nniikon <kolya.kasparov@gmail.com>
2024-04-02 19:53:04 +0300

    change Makefile for compatibility with clang

--- Makefile
+++ Makefile
@@ -4,6 +4,7 @@ all:
 	@$(MAKE) -C ./logs/
 	@$(MAKE) -C ./list/source/
 	@$(MAKE) -C ./hash_table/
+	@$(GXX) main.cpp $(CFLAGS) -c -o $(BUILD_DIR)/main.o
 
 clean:
 	@rm -rf $(BUILD_DIR)
--- config.mk
+++ config.mk
@@ -1,18 +1,17 @@
-CFLAGS = -O0 -Wall -Waggressive-loop-optimizations \
+CFLAGS = -O1 -Wall \
 -Wmissing-declarations -Wcast-align -Wcast-qual \
 -Wchar-subscripts -Wconversion \
 -Wempty-body -Wfloat-equal -Wformat-nonliteral -Wformat-security \
--Wformat-signedness -Wformat=2 -Winline -Wlogical-op \
--Wopenmp-simd -Wpacked -Wpointer-arith -Winit-self \
+-Wformat=2 -Winline \
+-Wpacked -Wpointer-arith -Winit-self \
 -Wredundant-decls -Wshadow \
--Wstrict-overflow=2 -Wsuggest-attribute=noreturn \
--Wsuggest-final-methods -Wsuggest-final-types \
--Wswitch-default -Wswitch-enum -Wsync-nand -Wundef -Wunreachable-code \
+-Wstrict-overflow=2 \
+-Wswitch-default -Wswitch-enum -Wundef -Wunreachable-code \
 -Wunused -Wvariadic-macros -Wno-nonnull \
 -Wno-missing-field-initializers -Wno-narrowing \
 -Wno-varargs -Wstack-protector -fcheck-new \
--fstack-protector -fstrict-overflow -flto-odr-type-merging \
--fno-omit-frame-pointer -Wstack-usage=8192 \
+-fstack-protector -fstrict-overflow \
+-fno-omit-frame-pointer \
 -fsanitize=address,bool,bounds,enum,float-cast-overflow,$\
 float-divide-by-zero,integer-divide-by-zero,leak,nonnull-attribute,null,$\
 object-size,return,returns-nonnull-attribute,shift,signed-integer-overflow,$\
@@ -26,4 +25,4 @@ export CFLAGS
 export BUILD_DIR = ${CURDIR}/build
 export EXEC_NAME = hash_table 
 
-export GXX = g++
+export GXX = clang++
